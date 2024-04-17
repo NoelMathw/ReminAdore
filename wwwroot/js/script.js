fa176b7adc930171367e892827d5d3fe865ac8c6
Noel Mathew <noel.varghese@students.plymouth.ac.uk>
2024-04-17 23:34:48 +0100

    Replacing 'Add birthdays' with a plus sign

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,16 +1,4 @@
-//class for creating accounts objects (used in sign up and login)
-/*class AccountsClass {
-    #name;
-    #DOB;
-    constructor(name, DOB) {
-        this.#name=name;
-        this.#DOB=DOB;
-    }
-    getUsername() {
-        return this.#username;
-    }
-}*/
-
+//Classes and objects
 //class template for to-do list
 class ToDoClass {
     ToDoString; //String variable to hold to-do list
@@ -20,8 +8,61 @@ class ToDoClass {
 class ReminderClass {
     #name;  //name of user
     #DOB; //DOB of the user
-    ToDo = [{}]; //To do list for each user //0th element will be naturally assigned to current user
+    ToDo = [{}]; //To do list for each user
 }
+
+
+//Code to create description animation
+const descriptions = [
+    "Never miss a friend's birthday again!",
+    "Your personal birthday reminder.",
+    "Your birthday reminder app.",
+    "Show your friends you care. Never miss a birthday.",
+    "Effortlessly remember birthdays."
+];
+let currentDescIndex = 0; // Keep track of the current description index
+async function updateDescription() {
+    const descriptionElement = document.querySelector(".description");
+    descriptionElement.innerText = descriptions[currentDescIndex]; // Update the text inside the description div
+    currentDescIndex = (currentDescIndex + 1) % descriptions.length; // Increment the index and loop back to 0 if it exceeds the array length
+}
+setInterval(updateDescription, 3000); // Set the interval to change description every 4000 milliseconds (4 seconds)
+updateDescription(); // Initial call to set the first description immediately
+
+
+//Element object of navigation buttons
+const Home = document.querySelector(".Home");
+const AllB = document.querySelector(".AllB");
+const AddB = document.querySelector(".AddB");
+
+
+//Functions which change page according to page user selects
+function runHomePage() {
+    Home.classList.add('active');
+    AllB.classList.remove('active');
+    AddB.classList.remove('active');
+} 
+
+function runAllBPage() {
+    AllB.classList.add('active');
+    Home.classList.remove('active');
+    AddB.classList.remove('active');
+}
+
+function runAddBPage() {
+    AddB.classList.add('active');
+    Home.classList.remove('active');
+    AllB.classList.remove('active');
+}
+
+
+//Event listeners for navigation buttons
+Home.addEventListener("click", runHomePage);
+AllB.addEventListener("click", runAllBPage);
+AddB.addEventListener("click", runAddBPage);
+
+runHomePage(); //First call to run the home page
+
 
 /*const body = document.querySelector(".body"); //Using this element to change body
     fetch('./JSON/pages.JSON')
@@ -39,21 +80,4 @@ class ReminderClass {
             console.error('Failed to load JSON:', error);
             body.innerHTML = '<p>Error loading page.</p>';
         });*/
-    //Code to create description animation
-    const descriptions = [
-        "Never miss a friend's birthday again!",
-        "Your personal birthday reminder.",
-        "Your birthday reminder app.",
-        "Show your friends you care. Never miss a birthday.",
-        "Effortlessly remember birthdays."
-    ];
-    let currentDescIndex = 0; // Keep track of the current description index
-
-    async function updateDescription() {
-        const descriptionElement = document.querySelector(".description");
-        descriptionElement.innerText = descriptions[currentDescIndex]; // Update the text inside the description div
-        currentDescIndex = (currentDescIndex + 1) % descriptions.length; // Increment the index and loop back to 0 if it exceeds the array length
-    }
-
-    setInterval(updateDescription, 3000); // Set the interval to change description every 4000 milliseconds (4 seconds)
-    updateDescription(); // Initial call to set the first description immediately
+    
