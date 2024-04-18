e082db669c3114c1d365f2534b9969a5f5617ac6
Noel Mathew <noel.varghese@students.plymouth.ac.uk>
2024-04-18 03:15:25 +0100

    Almost there (not really)

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -4,11 +4,54 @@ class ToDoClass {
     ToDoString; //String variable to hold to-do list
     ToDoDate; //
 }
+
 //class for creating reminder objects 
 class ReminderClass {
     #name;  //name of user
     #DOB; //DOB of the user
     ToDo = [{}]; //To do list for each user
+}
+
+//HTML object for adding reminder
+const AddBHTML = {
+    HTML: `
+<div class="d-flex justify-content-center align-items-center" style="min-height: 80vh;">
+    <div class="card shadow-lg p-3 mb-5 rounded newcard" style="width: 30rem;">
+        <div class="card-body">
+            <h3 class="card-title text-center mb-4">Add New Reminder</h3>
+            <form id="reminder-form">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Person's Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Enter name" required>
+                </div>
+                <div class="mb-3">
+                    <label for="dateOfBirth" class="form-label">Date of Birth</label>
+                    <input type="date" class="form-control" id="dateOfBirth" required>
+                </div>
+                <div class="mb-3">
+                    <label for="remindBefore" class="form-label">Remind me before (in days)</label>
+                    <input type="number" class="form-control" id="remindBefore" placeholder="Number of days" required>
+                </div>
+
+                <fieldset class="mb-4">
+                    <legend>To-Do List</legend>
+                    <div class="todo-items">
+                        <div class="todo-item mb-3">
+                            <label for="todo-1" class="form-label">To-Do Item</label>
+                            <input type="text" class="form-control todo-input" id="todo-1" placeholder="Enter a to-do item">
+                            <label for="todo-reminder-1" class="form-label mt-2">Reminder Date</label>
+                            <input type="date" class="form-control todo-reminder-input" id="todo-reminder-1">
+                        </div>
+                    </div>
+                    <button type="button" class="btn btn-outline-primary mt-2" id="add-todo-btn">
+                        <i class="fa fa-plus"></i> Add To-Do Item
+                    </button>
+                </fieldset>
+                <button type="submit" class="btn btn-primary w-100">Save Reminder</button>
+            </form>
+        </div>
+    </div>
+</div>`,
 }
 
 
@@ -29,7 +72,8 @@ async function updateDescription() {
 setInterval(updateDescription, 3000); // Set the interval to change description every 4000 milliseconds (4 seconds)
 updateDescription(); // Initial call to set the first description immediately
 
-
+//Element object of the main-content
+const MainContent = document.querySelector(".main-content");
 //Element object of navigation buttons
 const Home = document.querySelector(".Home");
 const AllB = document.querySelector(".AllB");
@@ -50,7 +94,7 @@ function runAllBPage() {
 }
 
 function runAddBPage() {
-    //addReminder();
+    MainContent.innerHTML = AddBHTML.HTML;
     runHomePage();
 }
 
